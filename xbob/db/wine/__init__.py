b0dbc8fb1ced4c718b3d0b714ab470b3f411174f
Andre Anjos <andre.anjos@idiap.ch>
2013-08-21 09:40:01 +0200

    [python3] Minor adjustments for py2/3 compatibility

--- xbob/db/wine/__init__.py
+++ xbob/db/wine/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
-# Sun 20 Jan 19:02:00 2013 CEST 
+# Sun 20 Jan 19:02:00 2013 CEST
 #
 # Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
 #
@@ -19,9 +19,9 @@
 
 """
 The Wine data set is a multivariate dataset introduced by M. Forina et al.
-These data are the results of a chemical analysis of wines grown in the 
-same region in Italy but derived from three different cultivars. The 
-analysis determined the quantities of 13 constituents found in each of 
+These data are the results of a chemical analysis of wines grown in the
+same region in Italy but derived from three different cultivars. The
+analysis determined the quantities of 13 constituents found in each of
 the three types of wines. It is hence often used for classification although
 it is not very challenging.
 
@@ -29,19 +29,19 @@ For more information: http://archive.ics.uci.edu/ml/datasets/Wine
 
 References:
 
-  1. Forina, M. et al (1988) "PARVUS - An Extendible Package for Data 
-  Exploration, Classification and Correlation." Institute of Pharmaceutical 
+  1. Forina, M. et al (1988) "PARVUS - An Extendible Package for Data
+  Exploration, Classification and Correlation." Institute of Pharmaceutical
   and Food Analysis and Technologies, Via Brigata Salerno, 16147 Genoa, Italy.
 
-  2. Aeberhard, S., Coomans, D., and de Vel, O. (1992) "Comparison of 
-  Classifiers in High Dimensional Settings". Tech. Rep. no. 92-02, 
-  Dept. of Computer Science and Dept. of Mathematics and Statistics, 
+  2. Aeberhard, S., Coomans, D., and de Vel, O. (1992) "Comparison of
+  Classifiers in High Dimensional Settings". Tech. Rep. no. 92-02,
+  Dept. of Computer Science and Dept. of Mathematics and Statistics,
   James Cook University of North Queensland.
   (Also submitted to Technometrics).
 
   3. Aeberhard, S., Coomans, D., and de Vel, O. (1992) "The Classification
-  Performance of RDA", Tech. Rep. no. 92-01, (1992), Dept. of Computer 
-  Science and Dept. of Mathematics and Statistics, James Cook University of 
+  Performance of RDA", Tech. Rep. no. 92-01, (1992), Dept. of Computer
+  Science and Dept. of Mathematics and Statistics, James Cook University of
   North Queensland.
   (Also submitted to Journal of Chemometrics).
 """
@@ -49,24 +49,24 @@ References:
 import os
 import sys
 import numpy
-from . import driver 
+from . import driver
 
-names = ['Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of Ash', 
-         'Magnesium', 'Total Phenols', 'Flavanoids', 'Nonflavanoid Phenols', 
-         'Proanthocyanins', 'Color intensity', 'Hue', 
+names = ['Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of Ash',
+         'Magnesium', 'Total Phenols', 'Flavanoids', 'Nonflavanoid Phenols',
+         'Proanthocyanins', 'Color intensity', 'Hue',
          'OD280/OD315 of Diluted Wines', 'Proline']
 """Names of the features for each entry in the dataset."""
 
 def data():
   """Loads from (text) file and returns Wine Dataset.
-  
+
   This set is small and simple enough to require an SQL backend. We keep the
   single file it has in text and load it on-the-fly every time this method is
   called.
 
   We return a dictionary containing the 3 classes of wines catalogued in
   this dataset. Each dictionary entry contains a 2D :py:class:`numpy.ndarray`
-  of 64-bit floats and they have respectively 59, 71 and 48 samples. Each 
+  of 64-bit floats and they have respectively 59, 71 and 48 samples. Each
   sample is an Array with 13 features as described by "names".
   """
   from .driver import Interface
@@ -74,14 +74,19 @@ def data():
 
   data = Interface().files()[0]
 
+  # The CSV file reader API changed between Python2 and Python3
+  open_dict = dict(mode='rb') #python2.x
+  if sys.version_info[0] >= 3: #python3.x
+    open_dict = dict(mode='rt', encoding='ascii', newline='')
+
   retval = {}
-  with open(data, 'rb') as csvfile:
+  with open(data, **open_dict) as csvfile:
     for row in csv.reader(csvfile):
       name = 'wine' + row[0][:].lower()
       retval.setdefault(name, []).append([float(k) for k in row[1:14]])
 
   # Convert to a float64 2D numpy.ndarray
-  for key, value in retval.iteritems():
+  for key, value in retval.items():
     retval[key] = numpy.array(value, dtype='float64')
 
   return retval
